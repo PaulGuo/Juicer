ee0696765dd74b854a70bb2655f62f6376b0cd12
GuoKai <badkaikai@gmail.com>
2012-04-01 18:01:18 +0800

    modified at  company.

--- juicer.js
+++ juicer.js
@@ -1,6 +1,6 @@
 (function() {
 	var recursive=function(str,o,c) {
-		var p=[],o=str.match(o)||[],c=str.match(c)||[];
+		var p=[],s=str,o=str.match(o)||[],c=str.match(c)||[];
 		var _build=function(str,o,i) {
 			var s=str.substr(0,str.indexOf(o[i]));
 			var e=str.substr(str.indexOf(o[i])+o[i].length);
@@ -12,30 +12,33 @@
 			return s.join('');
 		};
 		var _judge=function(arr) {
-			for(var i=0,c=0,t=[];i<arr.length;i++) {
+			for(var i=0,c=0,p=0,t=[];i<arr.length;i++) {
+				if(!c) p=i;
 				if(arr[i]=='o') c++;
 				if(arr[i]=='c') c--;
-				if(!c && arr[i]) t.push('oc');
+				if(!c && arr[i]) t.push(s.substr(p,i+1-p));
 			}
-			return !t.join('').match('co')?true:false;
+			return t;
 		};
 		for(var i=0;i<o.length;i++) {
 			p[str.indexOf(o[i])]='o';
 			str=_build(str,o,i);
 		}
 		for(var i=0;i<c.length;i++) {
-			p[str.indexOf(c[i])]='c';
+			p[str.indexOf(c[i])+c[i].length-1]='c';
 			str=_build(str,c,i);
 		}
+		console.log(p);
 		return _judge(p);
 	};
 
-	var pair=function(str,o,c) {
-		return str.match(o).length==str.match(c).length;
+	var replace=function(str,a,b) {
+		var s=str.substr(0,str.indexOf(a));
+		var e=str.substr(str.indexOf(a)+a.length);
+		return s+b+e;
 	};
 
 	this.juicer=function(tpl,data) {
-		//console.log(arguments);
 		var fn=arguments.callee;
 		var re={
 			'for':/{@for\s+([^}]*?)}([\s\S]*?){@\/for}/igm,
@@ -45,10 +48,7 @@
 			'_if':/{@if\s+([^}]*?)}([\s\S]*){@\/if}/igm
 		};
 		var _for=function($a,$1,$2) {
-			//console.log('------&&&&-----');
-			//console.log(arguments);
-			//console.log(recursive(arguments[arguments.length-1],/{@for[^}]*?}/igm,/{@\/for}/igm));
-			//console.log('------XXXX-----');
+			if(!data[$1]) return '';
 			for(var i=0,t=[];i<data[$1].length;i++) t.push(fn($2,data[$1][i]));
 			return t.join('');
 		};
@@ -61,11 +61,23 @@
 			return '';
 		};
 
-		tpl=tpl.replace(recursive(tpl,/{@for[^}]*?}/igm,/{@\/for}/igm)?re._for:re.for,_for);
-		tpl=tpl.replace(recursive(tpl,/{@if[^}]*?}/igm,/{@\/if}/igm)?re._if:re.if,_if);
+		//tpl=tpl.replace(recursive(tpl,/{@for[^}]*?}/igm,/{@\/for}/igm)?re._for:re.for,_for);
+		//tpl=tpl.replace(recursive(tpl,/{@if[^}]*?}/igm,/{@\/if}/igm)?re._if:re.if,_if);
+		//tpl=tpl.replace(re.unit,_unit);
+
+		var __f=recursive(tpl,/{@for[^}]*?}/igm,/{@\/for}/igm);
+		for(var i=0;i<__f.length;i++) {
+			tpl=replace(tpl,__f[i],__f[i].replace(re._for,_for));
+		}
+		var __i=recursive(tpl,/{@if[^}]*?}/igm,/{@\/if}/igm);
+		for(var i=0;i<__i.length;i++) {
+			tpl=replace(tpl,__i[i],__i[i].replace(re._if,_if));
+		}
 		tpl=tpl.replace(re.unit,_unit);
+
 		return tpl;
 	};
 
 	this.r=recursive;
+	this.p=replace;
 })();
