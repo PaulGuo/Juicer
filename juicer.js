9ff1d8da514e7f5f404342271226929d69b7601d
GuoKai <badkaikai@gmail.com>
2012-04-13 11:16:39 +0800

    improve a little performance.

--- juicer.js
+++ juicer.js
@@ -37,6 +37,8 @@
 	var __recursive=function(str,o,c) {
 		var p=[],s=str,o=str.match(o)||[],c=str.match(c)||[];
 		
+		if(!str || !o.length || !c.length) return [];
+		
 		var _build=function(str,o,i) {
 			var s=str.substr(0,str.indexOf(o[i]));
 			var e=str.substr(str.indexOf(o[i])+o[i].length);
@@ -58,7 +60,7 @@
 			}
 			return t;
 		};
-		
+		if(!str || !o.length || !c.length) return [];
 		for(var i=0;i<o.length;i++) {
 			p[str.indexOf(o[i])]='o';
 			str=_build(str,o,i);
@@ -79,6 +81,7 @@
 	};
 	
 	var __replaceAll=function(str,start,end,cb) {
+		if(!str) return str;
 		for(var i=0,__f=__recursive(str,start,end);i<__f.length;i++) {
 			str=__replace(str,__f[i],cb(__f[i]));
 		}
@@ -86,7 +89,8 @@
 	};
 
 	var __reference=function(str,re) {
-		var ret;
+		var ret=[];
+		if(!str) return ret;
 		str.replace(re,function() {
 			ret=arguments;
 			return ret[0];
