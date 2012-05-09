d530aea24ad05f041700522c496011430420a204
GuoKai <badkaikai@gmail.com>
2012-05-09 23:48:02 +0800

    update version after fixed issue #1.

--- yui-juicer.js
+++ yui-juicer.js
@@ -3,7 +3,7 @@
 	@email/gtalk: badkaikai@gmail.com
 	@blog/website: http://benben.cc
 	@license: apache license,version 2.0
-	@version: 0.3.0-dev
+	@version: 0.3.1-dev
 */
 
 YUI.add('juicer',function(Y) {
