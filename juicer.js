e99871ee0d53d134f97ffeb5cb45b1be3169f791
GuoKai <badkaikai@gmail.com>
2012-05-18 15:23:42 +0800

    backup.

--- juicer.js
+++ juicer.js
@@ -10,8 +10,14 @@
 	var juicer = function() {
 		var args = [].slice.call(arguments);
 		args.push(juicer.options);
-		if(arguments.length == 1) return juicer.compile.apply(juicer, args);
-		if(arguments.length >= 2) return juicer.to_html.apply(juicer, args);
+		
+		if(arguments.length == 1) {
+			return juicer.compile.apply(juicer, args);
+		}
+		
+		if(arguments.length >= 2) {
+			return juicer.to_html.apply(juicer, args);
+		}
 	};
 
 	var __escapehtml = {
@@ -33,8 +39,18 @@
 			return typeof(data) === 'undefined' ? '' : data;
 		}
 	};
+	
+	var __throw = function(error) {
+		if(console && console.warn) {
+			console.warn(error);
+			return;
+		}
+		
+		throw(error);
+	};
 
 	juicer.__cache = {};
+	
 	juicer.version = '0.4.0-dev';
 
 	juicer.settings = {
@@ -49,31 +65,50 @@
 		inlinecomment:  /{#[^}]*?}/igm,
 		rangestart:		/{@each\s*(\w*?)\s*in\s*range\((\d+?),(\d+?)\)}/igm
 	};
+	
+	juicer.helper = {};
+	
+	juicer.register = function(helper, body) {
+		if(this.helper[helper]) return;
+		this.helper[helper] = body;
+	};
 
 	juicer.options = {
 		cache: true,
 		strip: true,
 		errorhandling: true,
-		__escapehtml: __escapehtml
+		__escapehtml: __escapehtml,
+		__throw: __throw
 	};
 
 	juicer.set = function(conf, value) {
-		this.options[conf] = value;
+		if(arguments.length === 2) {
+			this.options[conf] = value;
+			return;
+		}
+		
+		if(conf === Object(conf)) {
+			for(var i in conf) {
+				if(conf.hasOwnProperty(i)) {
+					this.options[i] = conf[i];
+				}
+			}
+		}
 	};
 
 	juicer.template = function() {
-		var __this = this;
+		var that = this;
 
 		this.__interpolate = function(varname, escape, options) {
 			var __define = varname.split('|'), fn = '';
 			if(__define.length > 1) {
 				varname = __define.shift();
-				fn = '__method.' + __define.shift();
+				fn = '_method.' + __define.shift();
 			}
 			return '<%= ' +
-						(escape ? '__method.__escapehtml.__escape' : '') +
+						(escape ? '_method.__escapehtml.__escape' : '') +
 							'(' +
-								(!options || options.detection !== false ? '__method.__escapehtml.__detection' : '') +
+								(!options || options.detection !== false ? '_method.__escapehtml.__detection' : '') +
 									'(' +
 										fn +
 											'(' +
@@ -84,63 +119,63 @@
 					' %>';
 		};
 
-		this.__shell = function(tpl, options) {
-			var iterate_count = 0;
-			tpl = tpl
-				//for expression
-				.replace(juicer.settings.forstart, function($, varname, alias, key) {
-					var alias = alias || 'value', key = key && key.substr(1);
-					var iterate_var = 'i' + iterate_count++;
-					return '<% for(var ' + iterate_var + '=0, l' + iterate_var + '=' + varname + '.length;' + iterate_var + '<l' + iterate_var + ';' + iterate_var + '++) {' +
-								'var ' + alias + '=' + varname + '[' + iterate_var + '];' +
-								(key?('var ' + key + '=' + iterate_var + ';'):'') +
-							' %>';
-				})
-				.replace(juicer.settings.forend, '<% } %>')
-				//if expression
-				.replace(juicer.settings.ifstart, function($, condition) {
-					return '<% if(' + condition + ') { %>';
-				})
-				.replace(juicer.settings.ifend, '<% } %>')
-				//else expression
-				.replace(juicer.settings.elsestart, function($) {
-					return '<% } else { %>';
-				})
-				//else if expression
-				.replace(juicer.settings.elseifstart, function($, condition) {
-					return '<% } else if(' + condition + ') { %>';
-				})
-				//interpolate without escape
-				.replace(juicer.settings.noneencode, function($, varname) {
-					return __this.__interpolate(varname, false, options);
-				})
-				//interpolate with escape
-				.replace(juicer.settings.interpolate, function($, varname) {
-					return __this.__interpolate(varname, true, options);
-				})
-				//clean up comments
-				.replace(juicer.settings.inlinecomment, '')
-				//range expression
-				.replace(juicer.settings.rangestart, function($, varname, start, end) {
-					var iterate_var = 'j' + iterate_count++;
-					return '<% for(var ' + iterate_var + '=0;' + iterate_var + '<' + (end - start) + ';' + iterate_var + '++) {' +
-								'var ' + varname + '=' + iterate_var + ';' +
-							' %>';
-				});
+		this.__removeShell = function(tpl, options) {
+			var _counter = 0;
+			
+	juicer.register('forstart', function($, _name, alias, key) {
+		var alias = alias || 'value', key = key && key.substr(1);
+		var _iterate = 'i' + _counter++;
+		return '<% for(var ' + _iterate + '=0, l' + _iterate + '=' + _name + '.length;' + _iterate + '<l' + _iterate + ';' + _iterate + '++) {' +
+					'var ' + alias + '=' + _name + '[' + _iterate + '];' +
+					(key?('var ' + key + '=' + _iterate + ';'):'') +
+				' %>';
+	});
+	
+	juicer.register('forend', '<% } %>');
+	juicer.register('ifstart', function($, condition) {
+		return '<% if(' + condition + ') { %>';
+	});
+	juicer.register('ifend' ,'<% } %>');
+	juicer.register('elsestart', function($) {
+		return '<% } else { %>';
+	});
+	juicer.register('elseifstart', function($, condition) {
+		return '<% } else if(' + condition + ') { %>';
+	});
+	juicer.register('noneencode', function($, _name) {
+		return that.__interpolate(_name, false, options);
+	});
+	juicer.register('interpolate', function($, _name) {
+		return that.__interpolate(_name, true, options);
+	});
+	juicer.register('inlinecomment', '');
+	juicer.register('rangestart', function($, _name, start, end) {
+		var _iterate = 'j' + _counter++;
+		return '<% for(var ' + _iterate + '=0;' + _iterate + '<' + (end - start) + ';' + _iterate + '++) {' +
+					'var ' + _name + '=' + _iterate + ';' +
+				' %>';
+	});
+			
+			for(var helper in juicer.helper) {
+				if(juicer.helper.hasOwnProperty(helper)) {
+					tpl = tpl.replace(juicer.settings[helper], juicer.helper[helper]);
+				}
+			}
 
 			//exception handling
 			if(!options || options.errorhandling !== false) {
-				tpl = '<% try { %>' + tpl + '<% } catch(e) {console && console.warn("Juicer Render Exception: "+e.message);} %>';
+				tpl += '<% try { %>' + tpl;
+				tpl += '<% } catch(e) {__throw("Juicer Render Exception: "+e.message);} %>';
 			}
 
 			return tpl;
 		};
 
-		this.__pure = function(tpl, options) {
+		this.__toNative = function(tpl, options) {
 			return this.__convert(tpl, !options || options.strip);
 		};
 
-		this.__lexical = function(tpl) {
+		this.__lexicalAnalyze = function(tpl) {
 			var buffer = [];
 			var prefix = '';
 			var indexOf = function(array, item) {
@@ -154,63 +189,50 @@
 				
 				return -1;
 			};
-			var memo = function($, variable) {
-				variable = variable.match(/\w+/igm)[0];
+			var variableAnalyze = function($, statement) {
+				statement = statement.match(/\w+/igm)[0];
 				
-				if(indexOf(buffer,variable) === -1) {
-					buffer.push(variable); //fuck ie
+				if(indexOf(buffer, statement) === -1) {
+					buffer.push(statement); //fuck ie
 				}
 			};
 
-			tpl.replace(juicer.settings.forstart, memo).
-				replace(juicer.settings.interpolate, memo).
-				replace(juicer.settings.ifstart, memo);
+			tpl.replace(juicer.settings.forstart, variableAnalyze).
+				replace(juicer.settings.interpolate, variableAnalyze).
+				replace(juicer.settings.ifstart, variableAnalyze);
 
 			for(var i = 0;i < buffer.length; i++) {
-				prefix += 'var ' + buffer[i] + '=__data.' + buffer[i] + ';';
+				prefix += 'var ' + buffer[i] + '=_.' + buffer[i] + ';';
 			}
 			return '<% ' + prefix + ' %>';
 		};
 
 		this.__convert=function(tpl, strip) {
 			var buffer = [].join('');
-			buffer += "var __data = __data||{};";
+			buffer += "var _ = _ || {};";
 			buffer += "var out = '';out += '";
-			if(strip !== false) {
-				buffer += tpl
-						.replace(/\\/g, "\\\\")
-						.replace(/[\r\t\n]/g, " ")
-						.replace(/'(?=[^%]*%>)/g, "\t")
-						.split("'").join("\\'")
-						.split("\t").join("'")
-						.replace(/<%=(.+?)%>/g,"';out += $1;out += '")
-						.split("<%").join("';")
-						.split("%>").join("out += '")+
-						"';return out;";
-			} else {
-				buffer += tpl
-						.replace(/\\/g, "\\\\")
-						.replace(/[\r]/g, "\\r")
-						.replace(/[\t]/g, "\\t")
-						.replace(/[\n]/g, "\\n")
-						.replace(/'(?=[^%]*%>)/g, "\t")
-						.split("'").join("\\'")
-						.split("\t").join("'")
-						.replace(/<%=(.+?)%>/g,"';out += $1;out += '")
-						.split("<%").join("';")
-						.split("%>").join("out += '")+
-						"';return out.replace(/[\\r\\n]\\t+[\\r\\n]/g, '\\r\\n');";
-			}
+			buffer += tpl
+					.replace(/\\/g, "\\\\")
+					.replace(/[\r\t\n]/g, " ")
+					.replace(/'(?=[^%]*%>)/g, "\t")
+					.split("'").join("\\'")
+					.split("\t").join("'")
+					.replace(/<%=(.+?)%>/g,"';out += $1;out += '")
+					.split("<%").join("';")
+					.split("%>").join("out += '")+
+					"';return out;";
+					
 			return buffer;
 		};
 
 		this.parse = function(tpl, options) {
-			if(!options || options.loose !== false) tpl = this.__lexical(tpl) + tpl;
-			tpl = this.__shell(tpl, options);
-			tpl = this.__pure(tpl, options);
+			if(!options || options.loose !== false) tpl = this.__lexicalAnalyze(tpl) + tpl;
+			
+			tpl = this.__removeShell(tpl, options);
+			tpl = this.__toNative(tpl, options);
 			tpl = '"use strict";' + tpl; //use strict mode
 
-			this.render = new Function('__data, __method', tpl);
+			this.render = new Function('_, _method', tpl);
 			return this;
 		};
 	};
@@ -218,11 +240,20 @@
 	juicer.compile = function(tpl, options) {
 		try {
 			var engine = this.__cache[tpl] ? this.__cache[tpl] : new this.template().parse(tpl, options);
-			if(!options || options.cache !== false) this.__cache[tpl] = engine;
+			
+			if(!options || options.cache !== false) {
+				this.__cache[tpl] = engine;
+			}
+			
 			return engine;
 		} catch(e) {
-			console && console.warn('Juicer Compile Exception: ' + e.message);
-			return {render:function() {}};
+			__throw('Juicer Compile Exception: ' + e.message);
+			
+			return {
+				render: function() {
+					//noop
+				}
+			};
 		}
 	};
 
